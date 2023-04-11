74be3d567914fe2a050792df745f328b97e9a579
DwieTwarze2000 <pawelbinkowski2000@gmail.com>
2023-04-11 13:40:04 +0200

    searchBar finished

--- strona/frontend/src/main/searchBar/components/searchBar.component.jsx
+++ strona/frontend/src/main/searchBar/components/searchBar.component.jsx
@@ -6,50 +6,30 @@ const SearchBarComponent = () => {
     <div className="searchBar">
       <Row>
         <Col md={2}>
-          <div class="searchBar-tile">
+          <div className="searchBar-tile searchBar-tile-left">
             <label className="searchBar-label">Kraj</label>
             <br />
             <select className="searchBar-select">
-              <option selected disabled hidden>
-                Wybierz
-              </option>
+              <option selected>Wszytkie</option>
               <option>Włochy</option>
               <option>Francja</option>
               <option>Portugalia</option>
             </select>
           </div>
-          {/* <Form.Group class="form-floating">
-            <Form.Control as="select" id="floatingInput">
-              <option>Włochy</option>
-              <option>Francja</option>
-              <option>Portugalia</option>
-            </Form.Control>
-            <Form.Label for="floatingInput">Kraj</Form.Label>
-          </Form.Group>
-          <div class="form-floating mb-3"></div> */}
         </Col>
         <Col md={2}>
-          <div class="searchBar-tile">
-            <label className="searchBar-label">Ilość osób</label>
-            <br />
-            <input className="searchBar-select" type="number"></input>
-          </div>
-        </Col>
-        <Col md={2}>
-          <div class="searchBar-tile">
+          <div className="searchBar-tile">
             <label className="searchBar-label">Data</label>
             <br />
             <input className="searchBar-date" type="date"></input>
           </div>
         </Col>
         <Col md={2}>
-          <div class="searchBar-tile">
+          <div className="searchBar-tile">
             <label className="searchBar-label">Wyżywienie</label>
             <br />
             <select className="searchBar-select">
-              <option selected disabled hidden>
-                Wybierz
-              </option>
+              <option selected>Wszystkie</option>
               <option>All Inclusive</option>
               <option>W pyte</option>
               <option>Brak</option>
@@ -57,23 +37,32 @@ const SearchBarComponent = () => {
           </div>
         </Col>
         <Col md={2}>
-          <div class="searchBar-tile">
+          <div className="searchBar-tile">
             <label className="searchBar-label">Cel Wycieczki</label>
             <br />
             <select className="searchBar-select">
-              <option selected disabled hidden>
-                Wybierz
-              </option>
+              <option selected>Wszystkie</option>
               <option>Zwiedzanie</option>
               <option>Wypoczynek</option>
               <option>Chillera i utopia</option>
             </select>
           </div>
         </Col>
+        {/* cena od do */}
         <Col md={2}>
-          {/* <div class="searchBar-tile">
+          <div className="searchBar-tile">
+            <label className="searchBar-label">Cena za osobę (zł):</label>
+            <br />
+            Od <input className="searchBar-price" type="number"></input>
+            <br />
+            Do <input className="searchBar-price" type="number"></input>
+          </div>
+        </Col>
+
+        <Col md={2}>
+          <div className="searchBar-tile searchBar-button-tile">
             <button className="searchBar-button">Szukaj</button>
-          </div> */}
+          </div>
         </Col>
       </Row>
     </div>
